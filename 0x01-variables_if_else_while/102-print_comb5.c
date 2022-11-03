56ba790b398f999d55e7ad02329e45e4e8419eea
slyvanus <mensahnaphtali18@gmail.com>
2022-11-02 17:20:52 -0700

    variables

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,7 +9,7 @@
 int main(void)
 {
 	int i, j;
-	
+
 	for (i = 0; i < 100; i++)
 	{
 		for (j = 0; j < 100; j++)
@@ -29,8 +29,7 @@ int main(void)
 			}
 		}
 	}
-	
-	putchar('\n');
 
+	putchar('\n');
 	return (0);
 }
--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /*
- * main - prints all the numbers of base 16 in lowercase
+ * main - print all the numbers of base 16 in lowercase
  *
  * Return: Always 0 (Success)
  */
